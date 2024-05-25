240a63100e3692fd8c5790ba81342769767c2b2d
emmakodes <emmamichael65@gmail.com>
2024-05-25 15:34:54 +0100

    updated code

--- my_dataframe_chatbot/my_dataframe_chatbot.py
+++ my_dataframe_chatbot/my_dataframe_chatbot.py
@@ -1,108 +1,103 @@
 import reflex as rx
-
-from my_dataframe_chatbot import style
 from my_dataframe_chatbot.state import State
 
 
-def error_text() -> rx.Component:
-    """return a text component to show error."""
-    return rx.text(State.error_texts, text_align="center", font_weight="bold", color="red",)  
-
-
 def head_text() -> rx.Component:
-    """The header: return a text, text, divider"""
-    return rx.vstack(
-        rx.text("Chat with your data", font_size="2em", text_align="center", font_weight="bold", color="white",),
-        rx.text("(Note: input your openai api key, upload your csv file then click submit to start chat)", 
-                  text_align="center", color="white",),
-        rx.divider(border_color="white"),
+    """The header: return a text"""
+    return rx.flex(
+        rx.text("Chat with your data", size="4", align="center", weight="bold", color="white",),
+            justify_content="center",
+            width="100%",
+            margin_bottom="5px"
     )
-
-
-
-def openai_key_input() -> rx.Component:
-    """return a password component"""
-    return rx.password(
-            value=State.openai_api_key,
-            placeholder="Enter your openai key",
-            on_change=State.set_openai_api_key,
-            style=style.openai_input_style,
-    )
-
-
-color = "rgb(107,99,246)"
-
-
-def upload_csv():
-    """The upload component."""
-    return rx.vstack(
-        rx.upload(
-            rx.vstack(
-                rx.button(
-                    "Select File",
-                    color=color,
-                    bg="white",
-                    border=f"1px solid {color}",
-                ),
-                rx.text(
-                    "Drag and drop files here or click to select files"
-                ),
-                ),
-                multiple=False,
-                accept = {
-                    "text/csv": [".csv"],  # CSV format
-                },
-                max_files=1,
-                border=f"1px dotted {color}",
-                padding="2em",
-                ),
-                rx.hstack(rx.foreach(rx.selected_files, rx.text)),
-                rx.button(
-                    "Submit to start chat",
-                    on_click=lambda: State.handle_upload(
-                        rx.upload_files()
-                    ),
-                ),
-                padding="2em",
-            )
 
 
 def confirm_upload() -> rx.Component:
     """text component to show upload confirmation."""
-    return rx.text(State.upload_confirmation, text_align="center", font_weight="bold", color="green",)  
+    return rx.chakra.text(State.upload_confirmation, text_align="center", font_weight="bold", color="green")  
 
 
-def qa(question: str, answer: str) -> rx.Component:
-    """return the chat component."""
-    return rx.box(
-        rx.box(
-            rx.text(question, text_align="right", color="black"),
-            style=style.question_style,
-        ),
-        rx.box(
-                rx.text(answer, text_align="left", color="black"),
-                style=style.answer_style,
-        ),
-        margin_y="1em",
-    )
-
-
-def chat() -> rx.Component:
-    """iterate over chat_history."""
-    return rx.box(
-        rx.foreach(
-            State.chat_history,
-            lambda messages: qa(messages[0], messages[1]),
+def sidebar_drawer():
+    """the sidebar component"""
+    return rx.drawer.root(
+            rx.drawer.trigger(rx.button("Open Drawer", position="fixed",)),
+            rx.drawer.overlay(z_index="5"),
+            rx.drawer.portal(
+                rx.drawer.content(
+                    rx.flex(
+                        rx.drawer.close(rx.box(rx.button("Close"))),
+                        confirm_upload(),
+                        head_text(),
+                        rx.input(
+                                placeholder="Enter Openai API key",
+                                value=State.openai_api_key,on_change=State.set_openai_api_key,
+                                width="22em",  
+                                type="password",
+                                ),
+                        rx.vstack(
+                            rx.upload(
+                                rx.button("Select File", color="rgb(107,99,246)", bg="white", border=f"1px solid rgb(107,99,246)"),
+                                rx.text("Drag and drop files here or click to select files"),
+                                id="my_upload",
+                                border="1px dotted rgb(107,99,246)",
+                                padding="5em",
+                                multiple=False,
+                                accept = {
+                                    "text/csv": [".csv"],  # CSV format
+                                },
+                                max_files=1,
+                            ),                
+                            margin_top="3px"
+                        ),
+                        rx.hstack(rx.foreach(rx.selected_files("my_upload"), rx.text)),
+                        rx.button("Submit to start chat", variant="soft", margin_top="10px",on_click=lambda: State.handle_upload(
+                            rx.upload_files(upload_id="my_upload")
+                        ),),
+                        direction="column",
+                        align_items="start",
+                        padding="0.2em",
+                    ),
+                    top="auto",
+                    right="auto",
+                    height="100%",
+                    width="20em",
+                    padding_left="0.2em",
+                    background_color="#1a202c",
+                    color="white",
+                ),
+                direction="left",
+            )
         )
+
+
+def error_text():
+    """shown in case openai api key is not entered or file is not uploaded"""
+    return rx.chakra.vstack(
+        rx.chakra.modal(
+            rx.chakra.modal_overlay(
+                rx.chakra.modal_content(
+                    rx.chakra.modal_body(
+                        State.error_message
+                    ),
+                    rx.chakra.modal_footer(
+                        rx.chakra.button(
+                            "Close",
+                            on_click=State.change,
+                        )
+                    ),
+                )
+            ),
+            is_open=State.show,
+        ),
     )
 
 
-def loading_skeleton() -> rx.Component:
-    """return the skeleton component."""
-    return  rx.container(
-                rx.skeleton_circle(
+def skeleton_component() -> rx.Component:
+    """Skeleton is used to display the loading state of some components."""
+    return  rx.chakra.container(
+                rx.chakra.skeleton_circle(
                             size="30px",
-                            is_loaded=State.is_loaded_skeleton,
+                            is_loaded=State.is_skeleton_loaded,
                             speed=1.5,
                             text_align="center",
                         ),  
@@ -112,37 +107,123 @@ def loading_skeleton() -> rx.Component:
                     )
 
 
-
-def action_bar() -> rx.Component:
-    """return the chat input and ask button."""
-    return rx.hstack(
-        rx.input(
-            value=State.question,
-            placeholder="Ask a question about your data",
-            on_change=State.set_question,
-            style=style.input_style,
+def qa(question: str, answer: str) -> rx.Component:
+    """will show the question and answer"""
+    return rx.box(
+        rx.box(
+            rx.text(question, text_align="right", color="black"),
+            bg="#F5EFFE", margin_left="20%",
+            padding="1em",border_radius="5px",
+            margin_y="0.5em",box_shadow="rgba(0, 0, 0, 0.15) 0px 2px 8px",
         ),
-        rx.button(
-            "Ask",
-            on_click=State.answer,
-            style=style.button_style,
-        ),margin_top="3rem",
+        rx.cond(
+            answer,
+                rx.box(
+                    rx.text(answer, text_align="left", color="black"),
+                    bg="#DEEAFD", margin_right="20%",
+                    padding="1em",border_radius="5px",
+                    margin_y="0.5em",box_shadow="rgba(0, 0, 0, 0.15) 0px 2px 8px",
+                ),
+                rx.box()
+        ),
+        margin_y="1em",
     )
+
+
+def chat() -> rx.Component:
+    """iterate over chat_history."""
+    return  rx.box(
+                rx.foreach(
+                    State.chat_history,
+                    lambda messages: qa(messages[0], messages[1]),
+                ),
+                skeleton_component(),
+                py="8",
+                flex="1",
+                width="100%",
+                max_width=  ["15em", "20em", "40em", "40em", "40em", "50em"],
+                padding_x="4px",
+                align_self="center",
+                overflow="hidden",
+                padding_bottom="5em",
+                margin_left=["4em", "6em", "8em", "10em", "15em", "15em"]
+    )
+
+
+def hidden_button():
+    """use the id to make the scrollbar be at its point, done so that the scrollbar to always be at the bottom"""
+    return rx.flex(
+                id="downloadbutton",
+                background_color="black",
+                color="black",
+            )
+
+
+def chat_content():
+    return  rx.container(
+                error_text(),
+                chat(),
+                hidden_button(),
+            )
+
+
+def input_question_box():
+    """the input question box"""
+    return  rx.flex(
+                rx.text_area(
+                    value=State.question,
+                    placeholder="Ask a question about your data",
+                    on_change=State.set_question,
+                    width=["15em", "20em", "45em", "50em", "50em", "50em"],
+                    color="white",
+                    background_color="dark",
+                    height="auto",
+                ),
+                rx.button(
+                    rx.icon(
+                        "send", 
+                    ),
+                    rx.box(
+                        "Ask",
+                        as_="span"
+                    ),
+                    on_click=State.answer,
+                    size="2",
+                    variant="solid",
+                    color="white",
+                ),
+            direction="row",
+            align="end",
+            position="fixed",
+            bottom="0",
+            gap="8px",
+            padding="16px",
+            left="0",
+            right="0",
+            justify="center", 
+            margin="auto",
+            border_top=f"1px solid {rx.color('mauve', 3)}",
+            background_color=rx.color("mauve", 2),
+        )
 
 
 def index() -> rx.Component:
-    return rx.container(
-        error_text(),
-        head_text(),
-        openai_key_input(),
-        upload_csv(),
-        confirm_upload(),
-        chat(),
-        loading_skeleton(),
-        action_bar(),
-    )
+  return rx.flex(
+        sidebar_drawer(),
+        chat_content(),
+        input_question_box(),
+        direction="row",
+        gap="4px", 
+        min_height="100vh",
+        align_items="stretch",
+        spacing="0",
+  )
 
 
-app = rx.App()
+app = rx.App(
+    theme=rx.theme(
+        appearance="dark",
+    ),
+)
+
 app.add_page(index)
-app.compile()
--- my_dataframe_chatbot/state.py
+++ my_dataframe_chatbot/state.py
@@ -1,4 +1,4 @@
-# import reflex
+import os
 import reflex as rx
 
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
@@ -7,17 +7,14 @@ from langchain.agents.agent_types import AgentType
 
 import pandas as pd
 
-import os
-
 
 class State(rx.State):
 
     # The current question being asked.
     question: str
 
-    error_texts: str
+    error_message: str 
 
-    # Keep track of the chat history as a list of (question, answer) tuples.
     chat_history: list[tuple[str, str]]
 
     openai_api_key: str
@@ -29,66 +26,48 @@ class State(rx.State):
 
     file_path: str
 
-    is_loaded_skeleton: bool = True
+    is_skeleton_loaded: bool = True
 
+    show: bool = False
+            
 
-    async def handle_upload(
-        self, files: list[rx.UploadFile]
-    ):
-        """Handle the upload of file(s).
-
-        Args:
-            files: The uploaded files.
-        """
+    async def handle_upload(self, files: list[rx.UploadFile]):
         for file in files:
             upload_data = await file.read()
-            outfile = rx.get_asset_path(file.filename)
-            self.file_path = outfile
-
-            # Save the file.
-            with open(outfile, "wb") as file_object:
+            outfile = rx.get_upload_dir() / file.filename
+            self.file_path = str(outfile)
+            with outfile.open("wb") as file_object:
                 file_object.write(upload_data)
-
-            # Update the csv_file var.
-            self.csv_file.append(file.filename)
-
-            self.upload_confirmation = "csv file uploaded successfully, you can now interact with your data"
+        self.upload_confirmation = "csv file uploaded successfully, you can now interact with your data"
 
 
 
-    def answer(self):
-        # turn loading state of the skeleton component to False
-        self.is_loaded_skeleton = False
-        yield
+    async def answer(self):
+        yield rx.call_script(f"document.getElementById('downloadbutton').scrollIntoView();")
 
+        self.upload_confirmation = ""
 
         # check if openai_api_key is empty to return an error
         if self.openai_api_key == "":
-            self.error_texts = "enter your openai api"
+            self.error_message = "enter your openai api"
             return
-
-        # check if csv_file is empty to return an error
-        if not self.csv_file:
-            self.error_texts = "ensure you upload a csv file and enter your openai api key"
-            return
-
 
         if os.path.exists(self.file_path):
             df = pd.read_csv(self.file_path)
         else:
-            self.error_texts = "ensure you upload a csv file"
+            self.error_message = "ensure you upload a csv file"
             return
+        
+        # turn loading state of the skeleton component to False
+        self.is_skeleton_loaded = False
+        yield
 
         # initializes an agent for working with a chatbot and integrates it with a Pandas DataFrame
         agent = create_pandas_dataframe_agent(
                     ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key=self.openai_api_key),
                     df,
-                    verbose=True,
                     agent_type=AgentType.OPENAI_FUNCTIONS,
                 )
-
-
-        self.upload_confirmation = ""
 
         # Add to the answer as the chatbot responds.
         answer = ""
@@ -98,7 +77,7 @@ class State(rx.State):
         # run the agent against a question
         output = agent.run(self.question)
 
-        self.is_loaded_skeleton = True
+        self.is_skeleton_loaded = True
 
         # Clear the question input.
         self.question = ""
@@ -114,3 +93,7 @@ class State(rx.State):
                 answer,
             )
             yield
+
+
+    def change(self):
+        self.show = not (self.show)
